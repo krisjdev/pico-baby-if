5a01b1c9355f2f353bbd7ec4f01d3a8035b38645
kris j <kris.jurkans@gmail.com>
2024-06-29 16:25:16 +0100

    working example of clock generator and sampling input clock with edge detection

--- pico-baby-if.c
+++ pico-baby-if.c
@@ -1,62 +1,75 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
+#include <malloc.h>
+
 #include <babyif.h>
 
-#include <clocks.pio.h>
+// #include <clocks.pio.h>
+
 
 int main()
 {
     stdio_init_all();
 
-    PIO pio_clocks = pio0;
-    uint pio_clocks_offset = pio_add_program(pio_clocks, &clocks_program);
-    uint clock_freq = 1;
-    printf("loaded clock program at %d\n", pio_clocks_offset);
-    clocks_program_init(pio_clocks, 0, pio_clocks_offset);
-    pio_sm_put(pio_clocks, 0, (125000000 / (2 * clock_freq)) - 3);
-    pio_sm_set_enabled(pio_clocks, 0, true);
-
+    babyif *bif = babyif_constructor();
+    babyif_init(bif, pio0, pio1, 1);
     
+    babyif_init_pio_clock(bif);
+    babyif_enable_pio_clock(bif);
+
+    bool prev_clk_irq_0_val = false;
+    bool prev_baby_clk_gpio_val = false;
+    bool clk_irq_0 = false;
+    bool clk_irq_1 = false;
+    bool baby_clk_gpio_val = false;
+
 
     while (true) {
-        // printf("Hello, world!\n");
-        // sleep_ms(1000);
 
-        // hang until interrupt
-        // while (!pio_interrupt_get(pio_clocks, 0));
+        clk_irq_0 = babyif_get_generated_clock_irq(bif);
+        clk_irq_1 = babyif_get_baby_clock_irq(bif);
+        baby_clk_gpio_val = gpio_get(GPIO_IN_BABY_CLOCK_PIN);
 
-        // printf("interrupt 0 was set on pio? %s\n", pio_interrupt_get(pio_clocks, 0) ? "true" : "false");
-
-        // if (pio_interrupt_get(pio_clocks, 0)) {
-        //     printf("sleeping then clearing interrupt\n");
-        //     sleep_ms(250);
-        //     pio_interrupt_clear(pio_clocks, 0);
-        // }
-
-        // sleep_ms(750);
-
-        uint clk_irq_0 = pio_interrupt_get(pio_clocks, 0);
-        uint clk_irq_1 = pio_interrupt_get(pio_clocks, 1);
-
-        // hang until irq fires
-        // while (!clk_irq_0 || !clk_irq_1);
-
+        printf("---START\n");
         printf("irq0: %d , irq1: %d\n", clk_irq_0, clk_irq_1);
+
+            // 1      ->    0
+        if (baby_clk_gpio_val > prev_baby_clk_gpio_val) {
+            printf("\t\tbaby clock has gone high\n");
+
+            //     0              1
+        } else if (baby_clk_gpio_val < prev_baby_clk_gpio_val) {
+            printf("\t\tbaby clock has gone low\n");
+        } else if (baby_clk_gpio_val == prev_baby_clk_gpio_val) {
+            if (baby_clk_gpio_val == 1) {
+                printf("\t\tbaby clock has remained high\n");
+            } else {
+                printf("\t\tbaby clock has remained low\n");
+            }
+        }
+
+
+        // cannot use irq to check since it gets cleared
+        // and this loop runs much faster than the pio
+        // (or can do, depending on the freq settings)
+        // prev_baby_clk_gpio_val = gpio_get(bif->baby_clock_pin);
+        prev_baby_clk_gpio_val = gpio_get(GPIO_IN_BABY_CLOCK_PIN);
+
 
         if (clk_irq_0) {
             printf("\tclearing clk_irq_0\n");
             sleep_ms(250);
-            pio_interrupt_clear(pio_clocks, 0);
+            babyif_clear_generated_clock_irq(bif);
         }
 
         if (clk_irq_1) {
             printf("\tclearing clk_irq_1\n");
-            sleep_ms(250);
-            pio_interrupt_clear(pio_clocks, 1);
+            sleep_ms(500);
+            babyif_clear_baby_clock_irq(bif);
         }
 
+        printf("---END\n");
         sleep_ms(500);
-
     }
 }
