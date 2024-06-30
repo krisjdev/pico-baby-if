89a4b48c5c9debc08c28cdc8d994f16ec1b47124
kris j <kris.jurkans@gmail.com>
2024-06-30 18:07:49 +0100

    updated to test data io

--- pico-baby-if.c
+++ pico-baby-if.c
@@ -5,8 +5,6 @@
 
 #include <babyif.h>
 
-// #include <clocks.pio.h>
-
 
 int main()
 {
@@ -18,12 +16,18 @@ int main()
     babyif_init_pio_clock(bif);
     babyif_enable_pio_clock(bif);
 
+    babyif_init_pio_data(bif);
+    babyif_enable_pio_data(bif);
+
     bool prev_clk_irq_0_val = false;
     bool prev_baby_clk_gpio_val = false;
     bool clk_irq_0 = false;
     bool clk_irq_1 = false;
     bool baby_clk_gpio_val = false;
+    uint32_t val = 0;
 
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
 
     while (true) {
 
@@ -31,16 +35,37 @@ int main()
         clk_irq_1 = babyif_get_baby_clock_irq(bif);
         baby_clk_gpio_val = gpio_get(GPIO_IN_BABY_CLOCK_PIN);
 
+
         printf("---START\n");
-        printf("irq0: %d , irq1: %d\n", clk_irq_0, clk_irq_1);
+        printf("[clk] irq0: %d , irq1: %d\n", clk_irq_0, clk_irq_1);
+
+        printf("[data] irq_read: %d, irq_write: %d\n", babyif_get_received_data_irq(bif), babyif_get_sent_data_irq(bif));
+
+
+        // printf("write sm pc: %d\n", pio_sm_get_pc(bif->pio_data, 1));
+
+        // 00000000 00000000 00000000 00000000 
+        // 10101010_11110000_10101010_11110000 -> AAF0AAF0
+
+        // test if sending correct data by connecting to receiving pio
+        // babyif_put_data_word(bif, 0b00001111000010100000111100001010);
+        babyif_put_data_word(bif, 0x1234ABCD);
+        babyif_clear_sent_data_irq(bif);
+        
+        printf("rx word: %#10x", babyif_get_data_word(bif));
+
+        
 
             // 1      ->    0
         if (baby_clk_gpio_val > prev_baby_clk_gpio_val) {
             printf("\t\tbaby clock has gone high\n");
+            gpio_put(PICO_DEFAULT_LED_PIN, 1);
 
             //     0              1
         } else if (baby_clk_gpio_val < prev_baby_clk_gpio_val) {
             printf("\t\tbaby clock has gone low\n");
+            gpio_put(PICO_DEFAULT_LED_PIN, 0);
+
         } else if (baby_clk_gpio_val == prev_baby_clk_gpio_val) {
             if (baby_clk_gpio_val == 1) {
                 printf("\t\tbaby clock has remained high\n");
@@ -68,6 +93,8 @@ int main()
             sleep_ms(500);
             babyif_clear_baby_clock_irq(bif);
         }
+
+
 
         printf("---END\n");
         sleep_ms(500);
