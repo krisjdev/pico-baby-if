5b24dfb102a3f0aff5ef9d557a22857b36c05e33
kris j <kris.jurkans@gmail.com>
2024-07-22 19:42:31 +0100

    added new pin assignments

--- babyif/pindefs.h
+++ babyif/pindefs.h
@@ -16,3 +16,8 @@
 // https://pico.pinout.xyz/
 #define GPIO_IN_DATA_BASE_PIN 2     // GP2 to GP9 will be used for data input, paired with PTP_B
 #define GPIO_OUT_DATA_BASE_PIN 10   // GP10 to GP17 will be used for data output, paired with PTP_A
+
+#define GPIO_IN_RW_INTENT 22
+#define GPIO_OUT_RESET_N 26
+#define GPIO_OUT_ALLOW_EXEC 27
+#define GPIO_IN_STOP_LAMP 28
